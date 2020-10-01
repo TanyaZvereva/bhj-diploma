7b64b3ea558eca3c3e49e0cea20f15e42ae384b3
Tanya Zvereva <darkmarketgirl3@yandex.ru>
2020-10-01 15:47:46 +0300

    first commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,40 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
-};
+const createRequest = options => {
+    const xhr = new XMLHttpRequest,
+        formData = new FormData;
+    try {
+        if (options.method === 'GET') {
+            let str = "";
+            for (const key in options.data) {
+                if (str !== "") {
+                    str += "&";
+                }
+                str += key + "=" + options.data[key];
+            }
+            xhr.open('GET', `${options.url}?${str}`, true);
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.responseType = 'json';
+            xhr.onload = function () {
+                options.callback(null, xhr.response);
+            };
+            xhr.send();
+        } else {
+            xhr.open(options.method, options.url, true);
+            xhr.setRequestHeader('Accept', 'application/json');
+            xhr.responseType = 'json';
+            for (const key in options.data) {
+                formData.append(key, options.data[key]);
+            }
+            xhr.onload = function () {
+                options.callback(null, xhr.response);
+            };
+            xhr.send(formData);
+        };
+    } catch (e) {
+        // перехват сетевой ошибки
+        options.callback(e);
+    }
+}
