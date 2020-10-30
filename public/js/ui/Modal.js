bb07a33ab62112665eea919b22967ee6be93d5f0
TanyaZvereva <61490384+TanyaZvereva@users.noreply.github.com>
2020-10-30 17:39:45 +0300

    first commit modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,43 +12,57 @@ class Modal {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor( element ) {
-
+  constructor(element) {
+    if (element) {
+      this.element = element
+      this.allCloseElements = this.element.querySelectorAll('[data-dismiss = "modal"]')
+      this.registerEvents()
+    } else {
+      throw new Error("Ошибка!")
+    }
   }
-
   /**
    * При нажатии на элемент с data-dismiss="modal"
    * должен закрыть текущее окно
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    console.log(this.allCloseElements)
+    Array.from(this.allCloseElements).forEach(el => {
+      el.onclick = () => {
+        console.log(111)
+        this.onClose()
+      }
+    })
   }
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
    * Закрывает текущее окно (Modal.close())
    * */
-  onClose( e ) {
-
+  onClose(e) {
+    this.close()
   }
   /**
    * Удаляет обработчики событий
    * */
   unregisterEvents() {
-
+    Array.from(this.allCloseElements).forEach(el => {
+      el.onclick = null
+    })
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = "block"
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
-  close(){
-
+  close() {
+    console.log(this.element)
+    this.element.style.display = "none"
   }
 }
