57b84c47ecd52911c0e32788898b4de0755e2eff
TanyaZvereva <61490384+TanyaZvereva@users.noreply.github.com>
2020-10-30 17:48:06 +0300

    first commit userwidget.js

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -11,7 +11,11 @@ class UserWidget {
    * необходимо выкинуть ошибку.
    * */
   constructor( element ) {
-
+    if (element) {
+      this.element = element
+    } else {
+      throw new Error("Ошибка!")
+    }
   }
 
   /**
@@ -22,6 +26,9 @@ class UserWidget {
    * авторизованного пользователя
    * */
   update() {
-
+    const user = User.current()
+    console.log(user)
+    const userName = this.element.querySelector('.user-name')
+    userName.textContent = user.name
   }
 }
