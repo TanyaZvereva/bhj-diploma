c12b8cad0aa5b96e92026ffe571a681dff0c620a
TanyaZvereva <61490384+TanyaZvereva@users.noreply.github.com>
2020-11-05 19:09:57 +0300

    first commit createaccountform.js

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -3,7 +3,7 @@
  * создания нового счёта
  * Наследуется от AsyncForm
  * */
-class CreateAccountForm {
+class CreateAccountForm extends AsyncForm {
   /**
    * Создаёт счёт с помощью Account.create и закрывает
    * окно (в котором находится форма) в случае успеха,
@@ -11,6 +11,16 @@ class CreateAccountForm {
    * и сбрасывает форму
    * */
   onSubmit( options ) {
-
+    this.create(options.data, (err, response) => {
+      this.element.preventDefault()
+      this.element.reset()
+      console.log(response)
+      debugger
+        if (response.success) {
+         
+        } else {
+         
+        }
+      })
   }
 }
